3831df7f589336671782bdb65c570794fa391ca4
malaybiswal <malay.biswal@gmail.com>
2017-06-03 20:44:37 -0500

    Update loadCountingProfile.py

--- loadCountingProfile.py
+++ loadCountingProfile.py
@@ -18,7 +18,7 @@ Button2='Women'
 Button3='Senior'
 Button4='Kids'
 sql="""INSERT INTO CountingProfile(CustomerID,BusinessID,countingProfileName,countingMeasure,button1,button2,button3,button4) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)"""
-conn = pymysql.connect(host='censuscountersdev.czywg4t7fvct.us-east-1.rds.amazonaws.com', unix_socket='/tmp/mysql.sock', user='Census', passwd='CensusCountersDEV123=6', db='CensusCountersDEV',use_unicode=True, charset="utf8")
+conn = pymysql.connect(host='host', unix_socket='/tmp/mysql.sock', user='user', passwd='pwd', db='db',use_unicode=True, charset="utf8")
 cur = conn.cursor()
 
 #for i in range(1,38):
